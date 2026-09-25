f86294f32b12793c43119e811c4d41952d59c684
Surbhi Pandit <surbhipandit10134@gmail.com>
2026-09-25 09:43:24 +0530

    commit12-useNAviagte fix in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Navigate } from 'react-router-dom'
 import { Routes, Route } from 'react-router-dom'
 import LoginPage from './pages/LoginPage'
 import ProductsPage from './pages/ProductsPage'
